f52b5c6343564af45e931519959ed759f34ed395
Nuyj <New.10_04@icloud.com>
2017-06-26 18:30:43 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,5 +3,13 @@ for(var i =1; i < 101; i++){
   var li = document.createElement('li');
   li.textContent = i;
   document.getElementById('list').appendChild(li);
-}
+
+
+    for (var i = 1; i <= n; i++) {
+        var f = "";
+        if (i % 3 === 0) { f += "Fizz"; }
+        if (i % 5 === 0) { f += "Buzz"; }
+        if (s === "")    { f += i; }
+      
+    }
  
