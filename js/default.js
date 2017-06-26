bb73f2bce8cf24249981384f8a4bd19244747b81
Nuyj <New.10_04@icloud.com>
2017-06-26 18:13:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,9 +3,9 @@ for(var i =1; i < 101; i++){
   var li = document.createElement('li');
   li.textContent = i;
   document.getElementById('list').appendChild(li);
-  }
-if(i % 3 === 0){
+  if(i % 3 === 0){
   var li = document.createElement('li');
   li.textContent = Fizz;
   document.getElementById('list').appendChild(li);
+}
 }
